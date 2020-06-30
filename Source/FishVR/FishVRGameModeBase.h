86bb322cd96b334d303faacd7b5f0bca79c95f45
manzorsarahi <manzorsarahi@gmail.com>
2020-06-30 12:55:18 -0400

    Created project

---
+++ Source/FishVR/FishVRGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FishVRGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FISHVR_API AFishVRGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
